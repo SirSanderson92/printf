766513c33adeb8dc2bdfddb0cf78b80fa8def5ab
XceL92 <marvin.koriath@gmx.de>
2023-05-04 16:48:56 +0200

    dies ist eine Format Source Datei

---
+++ format.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = 42;
+	florat fnum = 42.00;
+	char chr = 'A';
+	char str [] = "Hello world!";
+
+	printf("Die Ganzzahl ist: %d\n", num);
+   	printf("Die Gleitkommazahl ist: %f\n", fnum);
+    	printf("Der Character ist: %c\n", chr);
+    	printf("Der String ist: %s\n", str);
+}
+
+
